0a185ee8034694f89d3d175789b4cc4b7ef53b44
Brandan Sayarath and K. Wicz <brandan+k.@>
2020-02-26 14:42:30 -0800

    add working functionality to Lookup() method.

--- Program.cs
+++ Program.cs
@@ -76,45 +76,62 @@ namespace Models
       {
         Console.WriteLine("------------------------");
         Console.WriteLine("Name: " + animal.GetName());
-        Console.WriteLine("Species : " + animal.GetSpecies());
-        Console.WriteLine("Gender : " + animal.GetGender());
-        Console.WriteLine("Diet : " + animal.GetDiet());
-        Console.WriteLine("Age : " + animal.GetAge());
+        Console.WriteLine("Species: " + animal.GetSpecies());
+        Console.WriteLine("Gender: " + animal.GetGender());
+        Console.WriteLine("Diet: " + animal.GetDiet());
+        Console.WriteLine("Age: " + animal.GetAge());
       }
       Init();
     }
-    
-
 
     public static void Lookup()
     {
-      Console.WriteLine("Would you like to lookup animal by name or species?");
-      string lookupBy = Console.ReadLine();
-      if (lookupBy == "name")
-      {
-        LookupByName();
-      }
-      else if (lookupBy == "species")
-      {
-        LookupBySpecies();
-      }
-      else 
-      {
-        Console.WriteLine("Sorry, please search by [name] or [species]");
-        Lookup();
-      }
-    }
-    public static void LookupByName()
-    {
       List<Animal> AnimalNameMatchingSearch = new List<Animal>(1);
+      Console.WriteLine("Would you like to lookup animal by name or species?");
+      string query = Console.ReadLine();
 
-      Console.WriteLine("What is the name of the animal you're looking for?");
+      Console.WriteLine("What is the " + query + " of the animal you're looking for?");
       string userAnimalLookup = Console.ReadLine();
+      
+      string result = "";
       foreach (Animal animal in Animals) 
       {
-        if (userAnimalLookup == animal.GetName())
+        if (query == "name")
         {
-          AnimalNameMatchingSearch.Add(animal); 
+          result = animal.GetName();
+          if (userAnimalLookup == result)
+          {
+            AnimalNameMatchingSearch.Add(animal); 
+          }
+        }
+        else if (query == "species")
+        {
+          result = animal.GetSpecies();
+          if (userAnimalLookup == result)
+          {
+            AnimalNameMatchingSearch.Add(animal); 
+          }
+        }
+        else if (query == "gender")
+        {
+          result = animal.GetGender();
+          if (userAnimalLookup == result)
+          {
+            AnimalNameMatchingSearch.Add(animal); 
+          }
+        }
+        else if (query == "diet")
+        {
+          result = animal.GetDiet();
+          if (userAnimalLookup == result)
+          {
+            AnimalNameMatchingSearch.Add(animal); 
+          }
+        }
+        else
+        {
+          Console.WriteLine("Sorry, that is not an option");
+          Lookup();
         }
       }
 
@@ -129,34 +146,12 @@ namespace Models
           Console.WriteLine("Diet : " + animal.GetDiet());
           Console.WriteLine("Age : " + animal.GetAge());
         }
+        Init();
       }
       else 
       {
         Console.WriteLine("Sorry, no animals match that name.");
-        LookupByName();
-      }
-    }
-  
-
-    public static void LookupBySpecies()
-    {
-      Console.WriteLine("What is the species of the animal you're looking for?");
-      string userAnimalLookup = Console.ReadLine();
-      foreach (Animal animal in Animals) 
-      {
-        if (userAnimalLookup == animal.GetSpecies())
-        {
-          Console.WriteLine("------------------------");
-          Console.WriteLine("Name: " + animal.GetName());
-          Console.WriteLine("Species : " + animal.GetSpecies());
-          Console.WriteLine("Gender : " + animal.GetGender());
-          Console.WriteLine("Diet : " + animal.GetDiet());
-          Console.WriteLine("Age : " + animal.GetAge());
-        }
-        else
-        {
-          Console.WriteLine("Sorry, cannot find that species.");
-        }
+        Lookup();
       }
     }
     
